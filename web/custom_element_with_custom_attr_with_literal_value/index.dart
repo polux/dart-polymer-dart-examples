9b0ed05808d425b08feffa91d303e3aa22d2939f
Seth Ladd <sethladd@google.com>
2013-08-04 16:59:41 -0700

    latest

--- web/custom_element_with_custom_attr_with_literal_value/index.dart
+++ web/custom_element_with_custom_attr_with_literal_value/index.dart
@@ -1,7 +1,6 @@
 import 'dart:html';
-import 'package:fancy_syntax/syntax.dart';
 
 main() {
-  TemplateElement.syntax['fancy'] = new FancySyntax();
-  query('#tmpl').model = '11';
+  TemplateElement template = query('#tmpl');
+  template.model = '11';
 }
