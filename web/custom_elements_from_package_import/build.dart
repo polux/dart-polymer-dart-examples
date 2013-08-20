96bc431d98ad83e888cc2c221ea1095e963bf184
Seth Ladd <sethladd@google.com>
2013-08-20 15:14:27 -0700

    try to see what build.dart would do

---
+++ web/custom_elements_from_package_import/build.dart
@@ -0,0 +1,9 @@
+import 'package:polymer/component_build.dart';
+import 'dart:io';
+
+// Testing: this should eventually produce warnings in the editor
+// when I do something wrong.
+
+main() {
+  build(new Options().arguments, ['index.html']);
+}
