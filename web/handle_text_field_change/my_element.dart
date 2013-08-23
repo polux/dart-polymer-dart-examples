2f6aa9d9e8307043596f4b5f9aa148769c919009
Seth Ladd <sethladd@google.com>
2013-08-23 13:33:04 -0700

    give example of bug where change event doesn't fire if input field has a bound value

--- web/handle_text_field_change/my_element.dart
+++ web/handle_text_field_change/my_element.dart
@@ -2,7 +2,9 @@ import 'package:polymer/polymer.dart';
 import 'dart:html';
 
 @CustomTag('my-element')
-class MyElement extends PolymerElement {
+class MyElement extends PolymerElement with ObservableMixin {
+  
+  @observable String thing;
   
   void handleChange(Event e, var detail, Node target) {
     print(e.target.value);
