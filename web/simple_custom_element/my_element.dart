b994d5da905bfb3a6f52c66651df92a5376393db
Seth Ladd <sethladd@google.com>
2013-08-25 11:38:21 +0200

    add example for calling method on custom element

--- web/simple_custom_element/my_element.dart
+++ web/simple_custom_element/my_element.dart
@@ -1,7 +1,7 @@
 import 'package:polymer/polymer.dart';
 
 // Custom elements extend PolymerElement.
-// Apparently, you also have to use an annotation to mark what tag this
+// You also have to use an annotation to mark what tag this
 // belongs to.
 @CustomTag("my-element")
 class MyElement extends PolymerElement {
