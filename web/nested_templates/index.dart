5ee76bbff4d8c65a0a24f975628b7093a05ab7a7
Seth Ladd <sethladd@google.com>
2013-08-26 11:28:38 +0200

    example of a nested template name access. doesn't work in polymer.dart

--- web/nested_templates/index.dart
+++ web/nested_templates/index.dart
@@ -1,8 +1,10 @@
 import 'dart:html';
+import 'package:fancy_syntax/syntax.dart';
 
 class Person {
   String name;
   Address address;
+  List colors;
 }
 
 class Address {
@@ -13,10 +15,17 @@ class Address {
 main() {
   var person = new Person()
       ..name = 'Bob Smith'
+      ..colors = ['red', 'yellow', 'orange']
       ..address = (new Address()
           ..city = 'Springfield'
           ..zip = '99999');
   
-  query('#tmpl').model = person;
-  query('#tmpl2').model = person;
+  TemplateElement tmpl = query('#tmpl') as TemplateElement;
+  TemplateElement tmpl2 = query('#tmpl') as TemplateElement;
+  
+  tmpl.bindingDelegate = new FancySyntax();
+  tmpl2.bindingDelegate = new FancySyntax();
+  
+  tmpl.model = person;
+  tmpl2.model = person;
 }
