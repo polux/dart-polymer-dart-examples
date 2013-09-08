5684ee4e80a7e1cc59d305e5f5c6c43022f4c5a4
Seth Ladd <sethladd@google.com>
2013-09-08 23:16:08 +0200

    example of using mutation observers

---
+++ web/mutation_observers/my_element.dart
@@ -0,0 +1,27 @@
+import 'package:polymer/polymer.dart';
+import 'dart:html';
+import 'dart:async';
+
+@CustomTag("my-element")
+class MyElement extends PolymerElement with ObservableMixin {
+  final List<String> timestamps = toObservable([]);
+  MutationObserver observer;
+  
+  created() {
+    super.created();
+    
+    observer = new MutationObserver(_onMutation);
+    observer.observe(shadowRoot.query('#timestamps'), childList: true, subtree: true);
+    
+    new Timer.periodic(const Duration(seconds: 1), (t) {
+      timestamps.add(new DateTime.now().toString());
+    });
+  }
+  
+  // Bindings, like repeat, happen asynchronously. To be notified
+  // when the shadow root's tree is modified, use a MutationObserver.
+  
+  _onMutation(List<MutationRecord> mutations, MutationObserver observer) {
+    print('${mutations.length} mutations occurred, the first to ${mutations[0].target}');
+  }
+}
