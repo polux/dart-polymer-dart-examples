c5e103e42398e9137b9b3cd7b4b044815055a02e
Seth Ladd <sethladd@google.com>
2013-09-09 16:40:45 +0200

    add doctype to appease linter

---
+++ build.dart
@@ -0,0 +1,13 @@
+#!/usr/bin/env dart
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+import 'package:polymer/component_build.dart';
+import 'package:polymer/deploy.dart' as deploy;
+
+void main() {
+  build(new Options().arguments, ['web/deploy_to_javascript/index.html'])
+      .then((_) => deploy.main());
+}
--- web/deploy_to_javascript/build.dart
+++
@@ -1,8 +0,0 @@
-import 'package:polymer/component_build.dart';
-import 'dart:io';
-
-main() {
-  var args = new Options().arguments;
-  args.addAll(['--', '--deploy']); // Note: the --deploy is what makes this work
-  build(args, ['index.html']);
-}
