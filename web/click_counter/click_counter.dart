e4bc5739f38051c7673d43eac0d7ecbcc3149798
Seth Ladd <sethladd@google.com>
2013-10-13 20:59:41 -0700

    add an attribute to the click-counter example

--- web/click_counter/click_counter.dart
+++ web/click_counter/click_counter.dart
@@ -3,7 +3,7 @@ import 'dart:html';
 
 @CustomTag('click-counter')
 class ClickCounterElement extends PolymerElement {
-  @observable int count = 0;
+  @published int count = 0;
   
   void increment(Event e, var detail, Node target) {
     count += 1;
