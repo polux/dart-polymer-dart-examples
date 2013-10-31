61ddd99020247d2b9513f5ad5827d8570f06ab75
Seth Ladd <sethladd@google.com>
2013-10-31 13:20:38 -0700

    simplify how to use filters

--- web/bind_number_to_text_field_with_filter/my_app.dart
+++ web/bind_number_to_text_field_with_filter/my_app.dart
@@ -1,6 +1,4 @@
 import 'package:polymer/polymer.dart';
-import 'package:polymer_expressions/polymer_expressions.dart';
-import 'package:template_binding/template_binding.dart';
 import 'dart:html';
 import 'models.dart';
 import 'ui_filters.dart';
@@ -11,21 +9,10 @@ class MyApp extends PolymerElement {
   
   MyApp.created() : super.created();
   
-  DocumentFragment instanceTemplate(Element template) {
-    return templateBind(template).createInstance(this, new _PolymerExpressionsWithEventDelegate(globals: {
-      'integer': new StringToInt()
-    }));
-  }
-  
   void clickMe(Event e, var detail, Node target) {
     print('clicked');
   }
-}
-
-class _PolymerExpressionsWithEventDelegate extends PolymerExpressions {
-  _PolymerExpressionsWithEventDelegate({Map globals}) : super(globals:globals);
-  getBinding(model, String path, name, node) {
-    return Polymer.getBindingWithEvents(
-        model, path, name, node, super.getBinding);
-  }
+  
+  // Filters and transformers can be referenced as fields.
+  final asInteger = new StringToInt();
 }
