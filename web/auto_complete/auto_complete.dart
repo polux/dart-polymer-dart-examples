64914d9bea0cebd7d21d4fd8d381469970e324f1
Seth Ladd <sethladd@google.com>
2013-11-04 17:42:50 -0800

    fix bug when auto_complete is used outside of main page

--- web/auto_complete/auto_complete.dart
+++ web/auto_complete/auto_complete.dart
@@ -8,28 +8,28 @@ class AutoCompleteElement extends PolymerElement {
   final List<String> haystack = [];
   bool skipSearch = false;
   int keyboardSelect = -1;
-  
+
   AutoCompleteElement.created() : super.created() {
-    UListElement dataSource = querySelector('.data-source') as UListElement;
+    UListElement dataSource = this.querySelector('.data-source') as UListElement;
     if (dataSource == null) {
       print("WARNING: expected to find a .data-source <ul> as a child");
       return;
     }
-    
+
     dataSource.children.forEach((LIElement e) {
       if (e is! LIElement) return;
       haystack.add(e.text);
     });
-    
+
     onPropertyChange(this, #search, _performSearch);
   }
-  
+
   void select(Event e, var detail, Node target) {
     search = target.text;
     _reset();
     skipSearch = true;
   }
-  
+
   _performSearch() {
     if (skipSearch) {
       skipSearch = false;
@@ -42,7 +42,7 @@ class AutoCompleteElement extends PolymerElement {
       return term.toLowerCase().startsWith(lower);
     }));
   }
-  
+
   keyup(KeyboardEvent e, var detail, Node target) {
     switch (new KeyEvent.wrap(e).keyCode) {
       case KeyCode.ESC:
@@ -59,14 +59,14 @@ class AutoCompleteElement extends PolymerElement {
         break;
     }
   }
-  
+
   _moveDown() {
     List<Element> lis = shadowRoot.querySelectorAll('ul li');
     if (keyboardSelect >= 0) lis[keyboardSelect].classes.remove('selecting');
     keyboardSelect = ++keyboardSelect == lis.length ? 0 : keyboardSelect;
     lis[keyboardSelect].classes.add('selecting');
   }
-  
+
   _moveUp() {
     List<Element> lis = shadowRoot.querySelectorAll('ul li');
     if (keyboardSelect >= 0) lis[keyboardSelect].classes.remove('selecting');
@@ -74,20 +74,20 @@ class AutoCompleteElement extends PolymerElement {
     keyboardSelect = --keyboardSelect == -1 ? lis.length-1 : keyboardSelect;
     lis[keyboardSelect].classes.add('selecting');
   }
-  
+
   _clear() {
     _reset();
     search = '';
     skipSearch = true;
   }
-  
+
   _select() {
     List<Element> lis = shadowRoot.querySelectorAll('ul li');
     search = lis[keyboardSelect].text;
     skipSearch = true;
     _reset();
   }
-  
+
   _reset() {
     keyboardSelect = -1;
     results.clear();
---
+++ web/auto_complete/my_app.dart
@@ -0,0 +1,7 @@
+import 'package:polymer/polymer.dart';
+
+@CustomTag('my-app')
+class MyApp extends PolymerElement {
+
+  MyApp.created() : super.created();
+}
